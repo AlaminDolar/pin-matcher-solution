be4f73e13138e933f90d72fd689a3bffa4ad0bab
AlaminDolar <amin15-9898@diu.edu.bd>
2022-05-16 21:24:33 +0600

    ok

--- js/app.js
+++ js/app.js
@@ -15,3 +15,37 @@ function generatePin(){
  document.getElementById('display-pin').value= pin;    
 
 }
+
+document.getElementById('key-pad').addEventListener('click',function(event){
+const number=event.target.innerText;
+const numberInput= document.getElementById('typed-numbers');
+if(isNaN(number)){
+  if(number == 'C'){
+      numberInput.value='';
+  }
+}
+else{
+    
+    const previousNumber = numberInput.value;
+    const newNumber= previousNumber+number;
+    numberInput.value= newNumber;
+}
+
+});
+
+function verifyPin(){
+    const pin =document.getElementById('display-pin').value;
+    const typedNumbers= document.getElementById('typed-numbers').value;
+    const notifySuccess=document.getElementById('notify-success');
+    const notifyFail=document.getElementById('notify-fail');
+    if (pin == typedNumbers){
+  
+      notifySuccess.style.display='block';
+      notifyFail.style.display='none';
+    }
+    else{
+       
+        notifyFail.style.display='block';
+        notifySuccess.style.display='none';
+    }
+}
